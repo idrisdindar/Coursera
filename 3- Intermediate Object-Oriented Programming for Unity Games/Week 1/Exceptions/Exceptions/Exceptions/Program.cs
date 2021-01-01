c4c298aed90a78b01a9925f87c4758deeb229cf2
IdrisDindar <idrisdindar@outlook.com>
2021-01-01 12:08:52 +0300

    Week 1 - Exceptions and File IO

---
+++ 3- Intermediate Object-Oriented Programming for Unity Games/Week 1/Exceptions/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    /// <summary>
+    /// Exceptions lectures code
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates exceptions and exception handling
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                // prompt for and get a short
+                Console.Write("Enter a whole number (-32,768 to 32,767): ");
+                short input = short.Parse(Console.ReadLine());
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Not a whole number!");
+            }
+            catch (OverflowException oe)
+            {
+                Console.WriteLine("That number is out of range!");
+            }
+            finally
+            {
+                Console.WriteLine("All done");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+
